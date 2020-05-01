b5a173128396819711e3749d4a12bcdcf797af4f
Liliana Gordon <53445418+fuzzylogicgrl@users.noreply.github.com>
2020-05-01 09:40:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
+<?php
+include "db.php";
+
+if(isset($_POST['submit'])){
+
+    $uname = mysqli_real_escape_string($conn,$_POST['txt_uname']);
+    $password = mysqli_real_escape_string($conn,$_POST['txt_pwd']);
+
+        $sql_query = "select * from users where username='$uname' and password='$password'";
+        $result = mysqli_query($conn,$sql_query);
+        $row = mysqli_fetch_assoc($result);
+
+        if($count > 0){
+            $_SESSION['uname'] = $row['uname'];
+            header('Location: home.php');
+        }else{
+            echo "Invalid username and password";
+        }
 
 
+}``
